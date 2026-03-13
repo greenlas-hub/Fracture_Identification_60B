6963869099dbcabbd29e6fdc49a559d499055ff4
jksandh2-ux <jksandh2@uci.edu>
2026-03-13 13:16:28 -0700

    add nrrd files

--- boneviewer.m
+++ boneviewer.m
@@ -1,5 +1,8 @@
 clear; clc; close all;
 
+%file = input("nrrd File to analyze: ","s");
+
+
 info = nrrdinfo("1533508883_5CORONAL-imageTypeDERIVED-PRIMARY-AXIAL-CT_SOM5SPO.nrrd");
 A = info.SpatialMapping.A;
 spacing = [norm(A(1:3,1)),norm(A(1:3,2)),norm(A(1:3,3))];
