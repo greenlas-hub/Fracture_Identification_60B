354c244b9ef0fb019b4adfbf09597134ff8f9e15
breannedlp330 <bedelape@uci.edu>
2026-03-12 20:11:15 -0700

    Update dicomread.m

--- dicomread.m
+++ dicomread.m
@@ -1,11 +1,11 @@
 clear; clc; close all;
 
-%% Loading the Data 
+% Loading the Data 
 info = nrrdinfo("1533508883_5CORONAL-imageTypeDERIVED-PRIMARY-AXIAL-CT_SOM5SPO.nrrd"); % Getting metadata
 A = info.SpatialMapping.A; % Performs voxel scaling & translation  
 spacing = [norm(A(1:3,1)),norm(A(1:3,2)),norm(A(1:3,3))];
 
-%% Load actual 3D volume into the workspace
+% Load actual 3D volume into the workspace
 medVol = medicalVolume("1533508883_5CORONAL-imageTypeDERIVED-PRIMARY-AXIAL-CT_SOM5SPO.nrrd");
 intensities = medVol.Voxels; % Obtain raw brightness values
 V = single(intensities); 
@@ -19,11 +19,12 @@ volshow(boneMask, ...
 boneMask = single(boneMask);
 bone = medicalVolume(boneMask, medVol.VolumeGeometry);
 
-%% Refining the Image 
+% Refining the Image 
 intensitiesSmooth = imgaussfilt3(bone.Voxels,2);
 volshow(intensitiesSmooth,...
     'Alphamap','linear',...
     'Transformation',makehgtform('scale',spacing([2,1,3])));
+
 
 
 
