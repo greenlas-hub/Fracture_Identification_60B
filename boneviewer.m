69ba89f7a020df62dca37021e08596d9fc4105a1
jksandh2-ux <jksandh2@uci.edu>
2026-03-13 12:28:35 -0700

    Add files via upload
    This works to view the bone and get a smoothed out model. I can make it into a function too.

---
+++ boneviewer.m
@@ -0,0 +1,25 @@
+clear; clc; close all;
+
+info = nrrdinfo("1533508883_5CORONAL-imageTypeDERIVED-PRIMARY-AXIAL-CT_SOM5SPO.nrrd");
+A = info.SpatialMapping.A;
+spacing = [norm(A(1:3,1)),norm(A(1:3,2)),norm(A(1:3,3))];
+transform = makehgtform('scale',spacing([2,1,3])); % create the transformation for each of the images to have the same custom spacing
+
+medVol = medicalVolume("1533508883_5CORONAL-imageTypeDERIVED-PRIMARY-AXIAL-CT_SOM5SPO.nrrd");
+intensities = medVol.Voxels;
+V = single(intensities);
+volshow(V,'Transformation',transform);
+
+%sliceViewer(V);
+
+mask = (V >= 300) & (V <= 2000); % based on HU unit for bones in CT scans, adjusted for visibility
+newV = zeros(size(V),'like',V); % creates newV with same spacing as V
+newV(mask) = 1; % every voxel of bone is white
+volshow(newV,... 
+        'Alphamap','linear', ...
+        'Transformation',transform);
+
+intensitiesSmooth = imgaussfilt3(newV,2);
+volshow(intensitiesSmooth,...
+    'Alphamap','linear',...
+    'Transformation',transform);
