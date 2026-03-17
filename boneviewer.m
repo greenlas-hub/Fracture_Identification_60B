03808e1cd90d73b651b220ffc8139b299c63f4fe
jksandh2-ux <jksandh2@uci.edu>
2026-03-17 12:39:35 -0700

    update

--- boneviewer.m
+++ boneviewer.m
@@ -10,12 +10,8 @@ A = info.SpatialMapping.A;
 spacing = [norm(A(1:3,1)),norm(A(1:3,2)),norm(A(1:3,3))];
 transform = makehgtform('scale',spacing([2,1,3])); % create the transformation for each of the images to have the same custom spacing
 
-medVol = medicalVolume(file);
-intensities = medVol.Voxels;
-V = single(intensities);
+V = nrrdread(file);
 volshow(V,'Transformation',transform);
-
-%sliceViewer(V);
 
 mask = (V >= 300) & (V <= 3000); % based on HU unit for bones in CT scans, adjusted for visibility
 newV = zeros(size(V),'like',V); % creates newV with same spacing as V
