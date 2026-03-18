f84d73c74ea1bf8882ff372e7bd5598827168c14
greenlas-hub <greenlas@uci.edu>
2026-03-18 00:41:52 -0700

    Update detect_fractures.m

--- detect_fractures.m
+++ detect_fractures.m
@@ -140,10 +140,10 @@ function results = detect_fractures(ds, roi, threshold)
         % figure out which streak comes first in the volume
         if bestStreakStart < secondStart
             gapStart = bestStreakEnd + 1;
-            gapEnd   = secondStart - 1;
+            gapEnd = secondStart - 1;
         else
             gapStart = secondEnd + 1;
-            gapEnd   = bestStreakStart - 1;
+            gapEnd = bestStreakStart - 1;
         end
 
         % mark slices in the gap as fracture if solidity dropped enough
@@ -216,20 +216,20 @@ function results = detect_fractures(ds, roi, threshold)
     end
 
     % pack everything into a struct so show_results can use it
-    results.subVolume      = subV;
-    results.boneMask       = boneMask;
+    results.subVolume = subV;
+    results.boneMask = boneMask;
     results.fractureSlices = fractureSlices;
-    results.gapPerSlice    = gapPerSlice;
-    results.solidityArr    = solidityArr;
-    results.areaArr        = areaArr;
-    results.piecesArr      = piecesArr;
-    results.baseline       = baseline;
-    results.threshold      = threshold;
-    results.roi            = roi;
-    results.spacing        = sp;
-    results.boneSlices     = boneSlices;
-    results.isHealthy      = isHealthy;
-    results.shaftRange     = [bestStreakStart, bestStreakEnd];
+    results.gapPerSlice = gapPerSlice;
+    results.solidityArr = solidityArr;
+    results.areaArr = areaArr;
+    results.piecesArr = piecesArr;
+    results.baseline = baseline;
+    results.threshold = threshold;
+    results.roi = roi;
+    results.spacing = sp;
+    results.boneSlices = boneSlices;
+    results.isHealthy = isHealthy;
+    results.shaftRange = [bestStreakStart, bestStreakEnd];
 end
 
 function [streakStart, streakLen, streakEnd] = findLongestStreak(healthyArr)
